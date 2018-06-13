1930b218b97e8652c5d4f4563d002c8df80f21af
greg <theawesomelifeofgreg@gmail.com>
2018-06-13 10:46:37 +0200

    init

---
+++ app/src/main/java/com/demo/room/MainActivity.java
@@ -0,0 +1,69 @@
+package com.demo.room;
+
+import android.arch.persistence.room.Room;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    FloatingActionButton fab;
+    EditText firstName;
+    EditText lastName;
+    AppDatabase db;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        db = Room.databaseBuilder(getApplicationContext(),
+                AppDatabase.class, "database-name").build();
+
+        firstName = findViewById(R.id.firstName);
+        lastName = findViewById(R.id.lastName);
+
+        TextView text = (TextView) findViewById(R.id.nameView);
+//        text = db.userDao().getAll();
+
+        fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                User user = new User(firstName.getText().toString(), lastName.getText().toString());
+                db.userDao().insertAll(user);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        User user = new User(firstName.getText().toString(), lastName.getText().toString());
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
